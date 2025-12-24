f1aa2e8c7287e61bd677281181b663856181466d
salem14-pro <muhammedsalem992@gmail.com>
2025-12-24 17:35:55 +0100

    good work

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ const io = socketIo(server, {
     cors: {
         origin: [
             'http://localhost:3000',
+            'http://localhost:5500',
+            'http://127.0.0.1:5500',
             'http://localhost:5000',
             'http://localhost:8000',
             'http://localhost:8001',
@@ -42,6 +44,8 @@ const upload = multer({
 app.use(cors({
     origin: [
         'http://localhost:3000',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
         'http://localhost:5000',
         'http://localhost:8000',
         'http://localhost:8001',
